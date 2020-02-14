868760ac5af808bff70a2cf371b3562d0c93771f
syedmrizwan <syed.m.rizwan@outlook.com>
2020-02-14 17:47:29 +0500

    Used NATS client to implement pub/sub model

---
+++ publisher/go.mod
@@ -0,0 +1,9 @@
+module pub
+
+go 1.13
+
+require (
+	github.com/nats-io/go-nats v1.7.2
+	github.com/nats-io/nkeys v0.1.3 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+)
---
+++ publisher/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/nats-io/go-nats"
+)
+
+// NOTE: Can test with demo servers.
+// nats-pub -s demo.nats.io <subject> <msg>
+// nats-pub -s demo.nats.io:4443 <subject> <msg> (TLS version)
+
+func usage() {
+	log.Printf("Usage: nats-pub [-s server] [-creds file] <subject> <msg>\n")
+	flag.PrintDefaults()
+}
+
+func main() {
+	var urls = flag.String("s", nats.DefaultURL, "The nats server URLs (separated by comma)")
+	var userCreds = flag.String("creds", "", "User Credentials File")
+
+	log.SetFlags(0)
+	flag.Usage = usage
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) != 2 {
+		usage()
+	}
+
+	// Connect Options.
+	opts := []nats.Option{nats.Name("NATS Sample Publisher")}
+
+	// Use UserCredentials
+	if *userCreds != "" {
+		opts = append(opts, nats.UserCredentials(*userCreds))
+	}
+
+	// Connect to NATS
+	nc, err := nats.Connect(*urls, opts...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	subj, msg := args[0], []byte(args[1])
+
+	nc.Publish(subj, msg)
+	nc.Flush()
+
+	if err := nc.LastError(); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("Published [%s] : '%s'\n", subj, msg)
+	}
+}
